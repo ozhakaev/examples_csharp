900ab453e7357077f1dbd369d817682c503ff221
Azamat Ozhakayev <ozhakaev@gmail.com>
2022-11-11 11:28:54 +0500

    example001hello_console

--- example001_HelloConsole/Program.cs
+++ example001_HelloConsole/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!TEST");
+Console.WriteLine("Hello, World!TEST123");
+Console.WriteLine("Hello, World!TEST123");
+Console.WriteLine("Hello, World!TEST123");
